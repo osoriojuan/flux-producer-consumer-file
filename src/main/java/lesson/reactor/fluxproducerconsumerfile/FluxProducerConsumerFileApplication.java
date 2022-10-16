754e0406915fbd0990d393e0b5855330e825b408
Juan Osorio Barrera <ext_josoriob@falabella.cl>
2022-10-16 19:26:38 -0300

    init commit

---
+++ src/main/java/lesson/reactor/fluxproducerconsumerfile/FluxProducerConsumerFileApplication.java
@@ -0,0 +1,13 @@
+package lesson.reactor.fluxproducerconsumerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxProducerConsumerFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxProducerConsumerFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lesson/reactor/fluxproducerconsumerfile/FluxProducerConsumerFileApplicationTests.java
@@ -0,0 +1,13 @@
+package lesson.reactor.fluxproducerconsumerfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxProducerConsumerFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
