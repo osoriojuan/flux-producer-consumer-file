af18eafbf50394c85a4607e02bd6654895cf94e8
Juan Osorio Barrera <ext_josoriob@falabella.cl>
2022-10-16 20:26:50 -0300

    solution

--- src/main/java/lesson/reactor/fluxproducerconsumerfile/FluxProducerConsumerFileApplication.java
+++ src/main/java/lesson/reactor/fluxproducerconsumerfile/FluxProducerConsumerFileApplication.java
@@ -1,13 +1,37 @@
 package lesson.reactor.fluxproducerconsumerfile;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
-@SpringBootApplication
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FluxProducerConsumerFileApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FluxProducerConsumerFileApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        getPublisher()
+                .log()
+                .subscribe();
+    }
+
+    private static Flux<String> getPublisher() throws IOException {
+        var file = new File("src/main/resources/FileToEmit");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        return Flux.create(synchronousSink -> {
+            String line;
+            try (fileReader){
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println("Emitting: ".concat(line));
+                    synchronousSink.next(line);
+                }
+            } catch (Exception e) {
+                synchronousSink.error(new IllegalAccessError("Error accessing to file: ".concat(e.getMessage())));
+            }
+            synchronousSink.complete();
+        });
+    }
+
 
 }
--- src/test/java/lesson/reactor/fluxproducerconsumerfile/FluxProducerConsumerFileApplicationTests.java
+++ src/test/java/lesson/reactor/fluxproducerconsumerfile/FluxProducerConsumerFileApplicationTests.java
@@ -1,13 +1,5 @@
 package lesson.reactor.fluxproducerconsumerfile;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class FluxProducerConsumerFileApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
